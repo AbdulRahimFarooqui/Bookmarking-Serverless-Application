425c6430b66e97ff18cd5cc6321b7ba072319053
AbdulRahimFarooqui <abdurraheemfarooqui@gmail.com>
2021-07-29 15:58:40 +0500

    ready to go

---
+++ functions/bookmarks/bookmarks.js
@@ -0,0 +1,95 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+require('events').EventEmitter.defaultMaxListeners = 400;
+const faunadb = require('faunadb'),
+  q = faunadb.query;
+
+const typeDefs = gql`
+  type Query {
+    bookmark: [Bookmark!]
+  }
+
+  type Bookmark {
+    id: ID!
+    url: String!
+    desc: String!
+  }
+
+  type Mutation {
+    addBookmark(url: String!, desc: String!) : Bookmark
+    deleteBookmark(id:ID!):String
+  }
+`
+let indexName;
+let lenCount;
+let deleteFromNewIndex = false;
+const resolvers = {
+  Query: {
+    bookmark: async (root, args, context) => {
+      try {
+        var client = new faunadb.Client({ secret: "fnAEO-aDOnACDGaja6B7fsgB_xz-rmf5DeRkOcBN" });
+        var result = await client.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index("url1"))),
+            q.Lambda(x => q.Get(x))
+          )
+        )
+        return result.data.map(d => {
+          return {
+            id: d.ts,
+            url: d.data.url,
+            desc: d.data.desc,
+          }
+        })
+      }
+      catch (err) {
+        console.log('err', err);
+      }
+    }
+  },
+  Mutation: {
+    addBookmark: async (_, { url, desc }) => {
+      try {
+        var client = new faunadb.Client({ secret: "fnAEO-aDOnACDGaja6B7fsgB_xz-rmf5DeRkOcBN" });
+        var result = await client.query(
+          q.Create(
+            q.Collection('links'),
+            {
+              data: {
+                url,
+                desc
+              }
+            },
+          )
+        );
+          return result.ref.data
+
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    deleteBookmark: async (_, { id }) => {
+      try {
+        var client = new faunadb.Client({ secret: "fnAEO-aDOnACDGaja6B7fsgB_xz-rmf5DeRkOcBN" });
+        const idNum = Number(id);
+          var result = await client.query(
+            q.Delete(
+              q.Select('ref',
+                q.Get(
+                  q.Match(q.Index('ts'), idNum)
+                )
+              )
+            )
+          )
+      }
+      catch (err) { console.log(err) }
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+exports.handler = server.createHandler()
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,89 @@
 import React from "react"
+import { useQuery, useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import deletePic from './delete-ico.png'
+
+const BookMarksQuery = gql`{
+  bookmark{
+    id
+    url
+    desc
+  }
+}`
+
+const AddBookMarkMutation = gql`
+  mutation addBookmark($url: String!, $desc: String!){
+    addBookmark(url: $url, desc: $desc){
+     url 
+    }
+}
+`
+
+const DeleteBookmark = gql`
+mutation deleteBookmark($id:ID!){
+  deleteBookmark(id:$id)
+}
+`;
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const { loading, error, data } = useQuery(BookMarksQuery);
+  const [addBookmark] = useMutation(AddBookMarkMutation);
+  const [deleteBookmark] = useMutation(DeleteBookmark);
+  let textfield;
+  let desc;
+  const addBookmarkSubmit = () => {
+    addBookmark({
+      variables: {
+        url: textfield.value,
+        desc: desc.value,
+      },
+      refetchQueries: [{ query: BookMarksQuery }],
+    })
+  }
+  if ((data !== undefined) && (data.bookmark.length !== 0)) {
+    return (
+      <div>
+        <div>
+          <input type="text" placeholder="URL" ref={node => textfield = node} />
+          <input type="text" placeholder="Description" ref={node => desc = node} />
+          <button onClick={addBookmarkSubmit}>Add BookMark</button>
+        </div>
+        <div>
+          <ul style={{ listStyle: "none", display: "inline-table", width: "600px" }}>
+            {data.bookmark.map((val, ind) => {
+              return (
+                <li key={ind}>
+                  <div style={{ border: "2px solid black", marginBottom: "1%", fontSize: 16, }}>
+                    <img style={{ marginLeft: "95%", marginTop: "2%" }} src={deletePic} height={20} onClick={async () => {
+                      await deleteBookmark({
+                        variables: {
+                          id: val.id,
+                        },
+                        refetchQueries: [{ query: BookMarksQuery }]
+                      })
+                    }
+                    } />
+                    <div>DESCRIPTION: <p style={{ fontSize: 16 }}>{val.desc}</p><br />URL:&nbsp;<a href={val.url}>{val.url}</a> </div>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <div>
+        <div>
+          Bookmarks' App.<br />Please Add any web address in the URL box and add any description to it.<br />
+          Remember! You need to keep the description unique
+        </div>
+        <input type="text" placeholder="URL" ref={node => textfield = node} />
+        <input type="text" placeholder="Description" ref={node => desc = node} />
+        <button onClick={addBookmarkSubmit}>Add BookMark</button>
+      </div>
+    </div>
+  )
 }
